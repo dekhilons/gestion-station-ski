0c0caed0c4e357798f18c53c5c26330dfa67602d
Malek Zitouni <115806147+malekzitouni10@users.noreply.github.com>
2024-11-05 11:44:40 +0100

    Update PisteRestController.java

--- src/main/java/tn/esprit/spring/controllers/PisteRestController.java
+++ src/main/java/tn/esprit/spring/controllers/PisteRestController.java
@@ -4,12 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.spring.dto.PisteDTO;
 import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.services.IPisteServices;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Tag(name = "\uD83C\uDFBF Piste Management")
 @RestController
@@ -21,51 +19,26 @@ public class PisteRestController {
 
     @Operation(description = "Add Piste")
     @PostMapping("/add")
-    public PisteDTO addPiste(@RequestBody PisteDTO pisteDTO) {
-        Piste piste = toEntity(pisteDTO);
-        Piste savedPiste = pisteServices.addPiste(piste);
-        return toDTO(savedPiste);
+    public Piste addPiste(@RequestBody Piste piste){
+        return  pisteServices.addPiste(piste);
     }
-
     @Operation(description = "Retrieve all Pistes")
     @GetMapping("/all")
-    public List<PisteDTO> getAllPistes() {
-        return pisteServices.retrieveAllPistes().stream()
-                .map(this::toDTO)
-                .collect(Collectors.toList());
+    public List<Piste> getAllPistes(){
+        return pisteServices.retrieveAllPistes();
     }
 
     @Operation(description = "Retrieve Piste by Id")
     @GetMapping("/get/{id-piste}")
-    public PisteDTO getById(@PathVariable("id-piste") Long numPiste) {
-        Piste piste = pisteServices.retrievePiste(numPiste);
-        return toDTO(piste);
+    public Piste getById(@PathVariable("id-piste") Long numPiste){
+        return pisteServices.retrievePiste(numPiste);
     }
 
     @Operation(description = "Delete Piste by Id")
     @DeleteMapping("/delete/{id-piste}")
-    public void deleteById(@PathVariable("id-piste") Long numPiste) {
+    public void deleteById(@PathVariable("id-piste") Long numPiste){
         pisteServices.removePiste(numPiste);
     }
+    
 
-    // Conversion methods
-    private PisteDTO toDTO(Piste piste) {
-        PisteDTO dto = new PisteDTO();
-        dto.setNumPiste(piste.getNumPiste());
-        dto.setNamePiste(piste.getNamePiste());
-        dto.setColor(piste.getColor().toString());
-        dto.setLength(piste.getLength());
-        dto.setSlope(piste.getSlope());
-        return dto;
-    }
-
-    private Piste toEntity(PisteDTO dto) {
-        Piste piste = new Piste();
-        piste.setNumPiste(dto.getNumPiste());
-        piste.setNamePiste(dto.getNamePiste());
-        piste.setColor(Color.valueOf(dto.getColor()));
-        piste.setLength(dto.getLength());
-        piste.setSlope(dto.getSlope());
-        return piste;
-    }
 }
