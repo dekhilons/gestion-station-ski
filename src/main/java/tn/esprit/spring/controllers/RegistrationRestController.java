df069a644e9487cfaf461298a596582c0d847d33
yahyaamri <113804678+yahyaamri@users.noreply.github.com>
2024-11-05 11:22:45 +0100

    Update RegistrationRestController.java

--- src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
+++ src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.entities.Registration;
 import tn.esprit.spring.entities.Support;
-import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.services.IRegistrationServices;
 
 import java.util.List;
