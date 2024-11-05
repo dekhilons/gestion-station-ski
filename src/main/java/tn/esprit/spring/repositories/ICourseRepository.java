86451e2df47b7beed7dbd51f3f47371e27369dab
yahyaamri <113804678+yahyaamri@users.noreply.github.com>
2024-11-05 11:27:05 +0100

    Update ICourseRepository.java

--- src/main/java/tn/esprit/spring/repositories/ICourseRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ICourseRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.spring.entities.Course;
 
 
-import java.util.List;
+
 
 public interface ICourseRepository extends JpaRepository<Course, Long> {
 
