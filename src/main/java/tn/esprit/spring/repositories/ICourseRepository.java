458084829e88b28c52e06f41fb303a8f73a87ee4
yahyaamri <113804678+yahyaamri@users.noreply.github.com>
2024-11-05 11:25:54 +0100

    Update ICourseRepository.java

--- src/main/java/tn/esprit/spring/repositories/ICourseRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ICourseRepository.java
@@ -3,7 +3,6 @@ package tn.esprit.spring.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-import org.springframework.data.repository.query.Param;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.TypeCourse;
 
